cc73b83cad4627c6a3a64a76586a3f1eb6b4ba97
criticalpixel <thomas@criticalpixel.com>
2017-02-09 20:48:41 +0000

    removed unused code

--- src/main.aot.ts
+++ src/main.aot.ts
@@ -1,4 +1,3 @@
-import 'core-js/es7/reflect';
 import { platformBrowser }    from '@angular/platform-browser';
 import { AppModuleNgFactory } from './ngfactory/src/app/app.module.ngfactory';
 import { enableProdMode } from '@angular/core';
